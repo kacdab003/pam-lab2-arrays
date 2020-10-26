35baf430f9e531f8215ca473d7bcf81a9146bc8f
Kacper Dąbrowski <kacperdabrowski@Kacpers-MacBook-Pro.local>
2020-10-26 18:17:18 +0100

    Initial commit

--- App.js
+++ App.js
@@ -3,9 +3,24 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const array = [];
+  let sum = 0;
+  for (let i = 0; i < 10; i++) {
+    array[i] = Number(Math.random() * 100);
+    sum += array[i];
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Tablica</Text>
+      <Text style={styles.text}>{array.map((i) => `${i.toFixed(2)}; `)}</Text>
+      <Text>Minimalna</Text>
+      <Text>{Math.min(...array).toFixed(2)}</Text>
+      <Text>Maksymalna</Text>
+      <Text>{Math.max(...array).toFixed(2)}</Text>
+      <Text>Średnia</Text>
+      <Text>{(sum / 10).toFixed(2)}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +32,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
   },
 });
